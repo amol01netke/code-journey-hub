a1898d6a53f8a1886a209dcfcb8382d3b619fe04
Amol Netke <amol01netke@gmail.com>
2024-01-18 00:31:37 +0530

    username

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,8 +5,8 @@ const userSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     profileURL: { type: String, required: true },
     codechef: {
